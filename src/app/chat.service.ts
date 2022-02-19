767c9bc4c8571a8342547faf4ec22cc7cf0e3d6b
Josefine <Paaske>
2022-02-19 16:46:10 +0100

    Does not connect to signalR do not know why

--- src/app/chat.service.ts
+++ src/app/chat.service.ts
@@ -1,9 +1,23 @@
 import { Injectable } from '@angular/core';
+import { HubConnectionBuilder } from '@microsoft/signalr';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ChatService {
+  private _hubConnection: any;
 
-  constructor() { }
+  constructor() { 
+    this._hubConnection = new HubConnectionBuilder()
+  .withUrl('https//localhost:5000/chathub')
+  .build();
+
+  this._hubConnection.on('ReceiveMessage', (message: any) => {
+    console.log(message);
+  });
+
+  this._hubConnection.start()
+  .then(() => console.log('connection started'))
+  .catch((err: string) => console.log('error while establishing signalr connection: ' + err));
+  }
 }
